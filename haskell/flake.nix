64a65d739256b3484796e467cbd22049f78965ad
Nikita Shumeiko <nikita.shumeiko@vitalbio.com>
2024-10-02 09:10:36 -0400

    nixfmt

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -4,24 +4,40 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [ inputs.haskell-flake.flakeModule ];
 
-      perSystem = { self', pkgs, system, ... }: {
-        formatter = nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
-        haskellProjects.default = {
-          basePackages = pkgs.haskellPackages;
+      perSystem =
+        {
+          self',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          formatter = nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
+          haskellProjects.default = {
+            basePackages = pkgs.haskellPackages;
 
-          devShell = {
-           enable = true;
-           tools = hp: { inherit (hp) fourmolu; ghcid = null; };
-           hlsCheck.enable = true;
+            devShell = {
+              enable = true;
+              tools = hp: {
+                inherit (hp) fourmolu;
+                ghcid = null;
+              };
+              hlsCheck.enable = true;
+            };
           };
-        };
 
-        packages.default = self'.packages.template;
-      };
+          packages.default = self'.packages.template;
+        };
     };
 }
